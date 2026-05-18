c90a35c6aa5a9ef20789a14480ab2cf6e7365593
anika-dot <heimani1@students.zhaw.ch>
2026-05-18 11:16:16 +0200

    reformatt with ruff

--- skripts/Class_PlayGame.py
+++ skripts/Class_PlayGame.py
@@ -78,8 +78,7 @@ class PlayGame(Game):
         os.system("cls" if os.name == "nt" else "clear")
 
         while self.running:
-            
-            print("\033[H", end="") # Move cursor to top-left instead of clearing
+            print("\033[H", end="")  # Move cursor to top-left instead of clearing
             self.display_board()
             self.car_step += 1
             self.counter += 1
@@ -113,8 +112,7 @@ class PlayGame(Game):
         os.system("cls" if os.name == "nt" else "clear")
 
         while self.running:
-            
-            print("\033[H", end="") # Move cursor to top-left instead of clearing
+            print("\033[H", end="")  # Move cursor to top-left instead of clearing
             self.display_board()
             self.car_step += 1
             self.counter += 1
